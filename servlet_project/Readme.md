790726f012c8cbcad4802404af20e350fd841eb7
Minglu Gu <gmlsyjsh@gmail.com>
2022-10-22 11:28:29 -0300

    servlet & tomcat configuration

---
+++ servlet_project/Readme.md
@@ -0,0 +1,22 @@
+1.  configuration files:
+    URL reference: https://cloud.google.com/appengine/docs/legacy/standard/java/configuration-files
+
+2.  web.xml
+    reference url: https://cloud.google.com/appengine/docs/legacy/standard/java/config/webxml#:~:text=and%20URL%20paths-,web.,method%20for%20HTTP%20GET%20requests.
+
+    web. xml defines mappings between URL paths and the servlets that handle requests with those paths. The web server uses this configuration to identify the servlet to handle a given request and call the class method that corresponds to the request method. For example: the doGet() method for HTTP GET requests.
+
+3.  内网IP
+    192.168*
+    10.*
+    172.16.* - 172.31.*
+    192.168.*
+
+    983 879 008
+
+4.  用Tomcat部署静态页面
+    1. 安装Tomcat
+    2. 在tomcat文件夹里面，找到apache-tomcat-8.5.83\bin 里面的startup.bat(for windows),拖到 cmd 里面。
+    3. 找到apache-tomcat-8.5.83\webapps文件夹，在该目录下，新建一个项目文件夹，来存放项目相关的文件并保存。
+    4. 在浏览器里输入项目相关的url链接，就可以在本地机器上面浏览了。例如：http://127.0.0.1:8080/blog102/blog_edit.html
+
---
+++ servlet_project/src/main/java/HelloServlet.java
@@ -0,0 +1,28 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// 把当前的 HelloServlet 类，和 http 请求的 URL 路径带有 /hello 这样的请求，就给关联起来了
+// 也要保证是 get 方法，方法要匹配（此处不能是post）
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    // override doGet(), do：处理，Get对应到HTTP的 GET 方法。
+    // 这个方法就是在 tomcat 收到了一个 HTTP GET 请求的时候，会被 Tomcat 调用到（回调函数）
+    // 请求：Request 响应：Response
+    // HttpServletRequest: 代表了 http 的请求。（Tomcat解析输入的数据）
+    // HttpServletResponse: 代表了 http 的响应 （根据请求）
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 这个代码一定要comment掉，不能调用父类的doGet()
+        // super.doGet(req, resp);
+        // 这个是服务器在控制台里面打印
+        System.out.println("hello world!");
+
+        // 在页面上也能打印 hello world. 把 hello world 字符串，放到 http 响应的 body 中。浏览器就会把 body 的内容显示到页面上
+        // resp: 响应的对象。getWriter(): 返回了一个 Write 对象，字符流对象
+        resp.getWriter().write("hello world!" + System.currentTimeMillis());
+    }
+}
