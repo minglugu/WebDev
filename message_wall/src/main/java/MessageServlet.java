e07931f3d760cc1b9d09832c72688baca26c5f6c
Minglu Gu <gmlsyjsh@gmail.com>
2022-10-24 20:15:18 -0300

    Tomcat server for message wall

---
+++ message_wall/src/main/java/MessageServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/message")
+public class MessageServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 处理提交消息的请求
+        resp.getWriter().write("hello, post");
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 获取消息的列表
+        resp.getWriter().write("hello, get");
+    }
+}
